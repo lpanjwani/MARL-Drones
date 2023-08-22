bb6fbe4735deeee7861bbcfbe85cd7b0899390a7
Lavesh <lpanjwani@outlook.com>
2023-08-22 05:40:58 +0100

    Organize SingleAgent PPO

--- scripts/learning/singleagent.py
+++ scripts/learning/singleagent.py
@@ -1,54 +1,22 @@
-"""Learning script for single agent problems.
-
-Agents are based on `stable_baselines3`'s implementation of  PPO, DDPG.
-
-Example
--------
-To run the script, type in a terminal:
-
-    $ python singleagent.py --env <env> --algo <alg> --obs <ObservationType> --act <ActionType> --cpu <cpu_num>
-
-Notes
------
-Use:
-
-    $ tensorboard --logdir ./results/save-<env>-<algo>-<obs>-<act>-<time-date>/tb/
-
-To check the tensorboard results at:
-
-    http://localhost:6006/
-
-"""
 import os
-from datetime import datetime
-from sys import platform
 import argparse
-import subprocess
-import numpy as np
+from datetime import datetime
 import gym
-import torch
-from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.cmd_util import (
-    make_vec_env,
-)  # Module cmd_util will be renamed to env_util https://github.com/DLR-RM/stable-baselines3/pull/197
-from stable_baselines3.common.vec_env import VecTransposeImage
-from stable_baselines3 import PPO
-from stable_baselines3 import DDPG
-from stable_baselines3.common.policies import ActorCriticPolicy as a2cppoMlpPolicy
-from stable_baselines3.common.policies import ActorCriticCnnPolicy as a2cppoCnnPolicy
-from stable_baselines3.td3 import MlpPolicy as td3ddpgMlpPolicy
-from stable_baselines3.td3 import CnnPolicy as td3ddpgCnnPolicy
-from stable_baselines3.common.callbacks import (
-    EvalCallback,
-    StopTrainingOnRewardThreshold,
-)
+from gym.spaces import Dict
+
+import ray
+from ray import tune
+from ray.tune import register_env
+from ray.rllib.agents import ppo
+from ray.rllib.agents.ppo import PPOTrainer
+from ray.rllib.utils.test_utils import check_learning_achieved
 
 from gym_pybullet_drones.utils.utils import str2bool
-
-from gym_pybullet_drones.envs.single_agent_rl.TakeoffAviary import TakeoffAviary
 from gym_pybullet_drones.envs.single_agent_rl.HoverAviary import HoverAviary
 from gym_pybullet_drones.envs.single_agent_rl.FlyThruGateAviary import FlyThruGateAviary
-from gym_pybullet_drones.envs.single_agent_rl.TuneAviary import TuneAviary
+from gym_pybullet_drones.envs.single_agent_rl.TakeoffAviary import TakeoffAviary
+
+
 from gym_pybullet_drones.envs.single_agent_rl.BaseSingleAgentAviary import (
     ActionType,
     ObservationType,
@@ -56,290 +24,249 @@ from gym_pybullet_drones.envs.single_agent_rl.BaseSingleAgentAviary import (
 
 import shared_constants
 
-EPISODE_REWARD_THRESHOLD = -0  # Upperbound: rewards are always negative, but non-zero
-"""float: Reward threshold to halt the script."""
-
-DEFAULT_ENV = "hover"
-DEFAULT_ALGO = "ppo"
-DEFAULT_OBS = ObservationType("kin")
-DEFAULT_ACT = ActionType("one_d_rpm")
-DEFAULT_CPU = 1
-DEFAULT_STEPS = 35000
-DEFAULT_OUTPUT_FOLDER = "results"
-DEFAULT_GUI = False
-DEFAULT_RECORD_VIDEO = True
+OWN_OBSERVATION_VEC_SIZE = 12
+ACTION_VECTOR_SIZE = 4
 
 
-def run(
-    env=DEFAULT_ENV,
-    algo=DEFAULT_ALGO,
-    obs=DEFAULT_OBS,
-    act=DEFAULT_ACT,
-    cpu=DEFAULT_CPU,
-    steps=DEFAULT_STEPS,
-    output_folder=DEFAULT_OUTPUT_FOLDER,
-    gui=DEFAULT_GUI,
-    record_video=DEFAULT_RECORD_VIDEO,
-):
-    #### Save directory ########################################
-    filename = os.path.join(
-        output_folder,
-        "save-"
-        + env
-        + "-"
-        + algo
-        + "-"
-        + obs.value
-        + "-"
-        + act.value
-        + "-"
-        + datetime.now().strftime("%m.%d.%Y_%H.%M.%S"),
-    )
-    if not os.path.exists(filename):
-        os.makedirs(filename + "/")
+class SingleAgentPPO:
+    args = None
+    environment = None
+    environment_name = None
+    num_drones = 1
+    results_directory = None
+    tuner_config = None
+    tuner_results = None
+    tuner_stopper = None
 
-    #### Print out current git commit hash #####################
-    if (platform == "linux" or platform == "darwin") and (
-        "GITHUB_ACTIONS" not in os.environ.keys()
-    ):
-        git_commit = subprocess.check_output(
-            ["git", "describe", "--tags", "--always"]
-        ).strip()
-        with open(filename + "/git_commit.txt", "w+") as f:
-            f.write(str(git_commit))
+    def __init__(self):
+        self.args = self.parse_cli_arguments()
+        self.results_directory = self.create_results_directory(self.args)
+        self.build_action_vector_size(self.args)
+        self.init_ray()
+        self.register_gym_environment(self.args)
+        self.build_tuner_config(self.args)
+        self.build_tuner_stop_conditions()
+        self.run_tuner(self.results_directory, self.tuner_config, self.tuner_stopper)
+        self.check_learning_achieved(self.tuner_results)
+        self.build_path_file(self.results_directory, self.tuner_results)
+        self.shutdown_ray()
 
-    #### Warning ###############################################
-    if env == "tune" and act != ActionType.TUN:
-        print(
-            "\n\n\n[WARNING] TuneAviary is intended for use with ActionType.TUN\n\n\n"
+    # Parse CLI arguments
+    def parse_cli_arguments(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--env",
+            default="hover",
+            type=str,
+            choices=["hover", "flythry", "takeoff"],
+            help="Task (default: hover)",
+            metavar="",
         )
-    if (
-        act == ActionType.ONE_D_RPM
-        or act == ActionType.ONE_D_DYN
-        or act == ActionType.ONE_D_PID
-    ):
-        print("\n\n\n[WARNING] Simplified 1D problem for debugging purposes\n\n\n")
-        #### Errors ################################################
-        if not env in ["takeoff", "hover"]:
-            print(
-                "[ERROR] 1D action space is only compatible with Takeoff and HoverAviary"
-            )
+        parser.add_argument(
+            "--obs",
+            default="kin",
+            type=ObservationType,
+            help="Observation space (default: kin)",
+            metavar="",
+        )
+        parser.add_argument(
+            "--act",
+            default=ActionType.RPM,
+            type=ActionType,
+            help="Action space (default: RPM)",
+            metavar="",
+        )
+        parser.add_argument(
+            "--algo",
+            default="cc",
+            type=str,
+            choices=["cc"],
+            help="MARL approach (default: cc)",
+            metavar="",
+        )
+        parser.add_argument(
+            "--workers",
+            default=0,
+            type=int,
+            help="Number of RLlib workers (default: 0)",
+            metavar="",
+        )
+        parser.add_argument(
+            "--gui",
+            default=True,
+            type=str2bool,
+            help="Whether to use PyBullet GUI (default: True)",
+            metavar="",
+        )
+        parser.add_argument(
+            "--record_video",
+            default=False,
+            type=str2bool,
+            help="Whether to record a video (default: False)",
+            metavar="",
+        )
+
+        ARGS = parser.parse_args()
+
+        return ARGS
+
+    # Create results directory with timestamp
+    def create_results_directory(self, ARGS):
+        filename = (
+            os.path.dirname(os.path.abspath(__file__))
+            + "/results/save-"
+            + ARGS.env
+            + "-"
+            + str(1)
+            + "-"
+            + ARGS.algo
+            + "-"
+            + ARGS.obs.value
+            + "-"
+            + ARGS.act.value
+            + "-"
+            + datetime.now().strftime("%m.%d.%Y_%H.%M.%S")
+        )
+        if not os.path.exists(filename):
+            os.makedirs(filename + "/")
+
+        return filename
+
+    # Build action constants
+    def build_action_vector_size(self, ARGS):
+        if ARGS.act in [
+            ActionType.ONE_D_RPM,
+            ActionType.ONE_D_DYN,
+            ActionType.ONE_D_PID,
+        ]:
+            ACTION_VECTOR_SIZE = 1
+        elif ARGS.act in [ActionType.RPM, ActionType.DYN, ActionType.VEL]:
+            ACTION_VECTOR_SIZE = 4
+        elif ARGS.act == ActionType.PID:
+            ACTION_VECTOR_SIZE = 3
+        else:
+            print("[ERROR] unknown ActionType")
             exit()
-    if act == ActionType.TUN and env != "tune":
-        print("[ERROR] ActionType.TUN is only compatible with TuneAviary")
-        exit()
-    if algo in ["sac", "td3", "ddpg"] and cpu != 1:
-        print("[ERROR] The selected algorithm does not support multiple environments")
-        exit()
 
-    #### Uncomment to debug slurm scripts ######################
-    # exit()
+    # Initialize Ray Server
+    def init_ray(self):
+        self.shutdown_ray()
+        ray.init(ignore_reinit_error=True)
 
-    env_name = env + "-aviary-v0"
-    sa_env_kwargs = dict(
-        aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
-        obs=obs,
-        act=act,
-        gui=gui,
-        record=record_video,
-    )
-    # train_env = gym.make(env_name, aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS, obs=obs, act=act) # single environment instead of a vectorized one
-    if env_name == "takeoff-aviary-v0":
-        train_env = make_vec_env(
-            TakeoffAviary, env_kwargs=sa_env_kwargs, n_envs=cpu, seed=0
-        )
-    if env_name == "hover-aviary-v0":
-        train_env = make_vec_env(
-            HoverAviary, env_kwargs=sa_env_kwargs, n_envs=cpu, seed=0
-        )
-    if env_name == "flythrugate-aviary-v0":
-        train_env = make_vec_env(
-            FlyThruGateAviary, env_kwargs=sa_env_kwargs, n_envs=cpu, seed=0
-        )
-    if env_name == "tune-aviary-v0":
-        train_env = make_vec_env(
-            TuneAviary, env_kwargs=sa_env_kwargs, n_envs=cpu, seed=0
-        )
-    print("[INFO] Action space:", train_env.action_space)
-    print("[INFO] Observation space:", train_env.observation_space)
-    # check_env(train_env, warn=True, skip_render_check=True)
+    # Shutdown Ray Server
+    def shutdown_ray(self):
+        ray.shutdown()
 
-    #### On-policy algorithms ##################################
-    onpolicy_kwargs = dict(
-        activation_fn=torch.nn.ReLU,
-        net_arch=[512, 512, dict(vf=[256, 128], pi=[256, 128])],
-    )  # or None
+    def register_gym_environment(self, ARGS):
+        if ARGS.env == "hover":
+            return self.register_hover_environment(ARGS)
+        elif ARGS.env == "flythru":
+            return self.register_flythru_environment(ARGS)
+        elif ARGS.env == "takeoff":
+            return self.register_takeoff_environment(ARGS)
+        else:
+            print("[ERROR] environment not yet implemented")
+            exit()
 
-    if algo == "ppo":
-        model = (
-            PPO(
-                a2cppoMlpPolicy,
-                train_env,
-                policy_kwargs=onpolicy_kwargs,
-                tensorboard_log=filename + "/tb/",
-                verbose=1,
-            )
-            if obs == ObservationType.KIN
-            else PPO(
-                a2cppoCnnPolicy,
-                train_env,
-                policy_kwargs=onpolicy_kwargs,
-                tensorboard_log=filename + "/tb/",
-                verbose=1,
-            )
+    def register_hover_environment(self, ARGS):
+        self.environment_name = "hover-aviary-v0"
+
+        register_env(
+            self.environment_name,
+            lambda _: HoverAviary(
+                aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
+                obs=ARGS.obs,
+                act=ARGS.act,
+            ),
         )
 
-    #### Off-policy algorithms #################################
-    offpolicy_kwargs = dict(
-        activation_fn=torch.nn.ReLU, net_arch=[512, 512, 256, 128]
-    )  # or None # or dict(net_arch=dict(qf=[256, 128, 64, 32], pi=[256, 128, 64, 32]))
-    if algo == "ddpg":
-        model = (
-            DDPG(
-                td3ddpgMlpPolicy,
-                train_env,
-                policy_kwargs=offpolicy_kwargs,
-                tensorboard_log=filename + "/tb/",
-                verbose=1,
-            )
-            if obs == ObservationType.KIN
-            else DDPG(
-                td3ddpgCnnPolicy,
-                train_env,
-                policy_kwargs=offpolicy_kwargs,
-                tensorboard_log=filename + "/tb/",
-                verbose=1,
-            )
-        )
-
-    #### Create eveluation environment #########################
-    if obs == ObservationType.KIN:
-        eval_env = gym.make(
-            env_name,
+        self.environment = HoverAviary(
             aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
-            obs=obs,
-            act=act,
+            obs=ARGS.obs,
+            act=ARGS.act,
+            gui=ARGS.gui,
+            record=ARGS.record_video,
         )
-    elif obs == ObservationType.RGB:
-        if env_name == "takeoff-aviary-v0":
-            eval_env = make_vec_env(
-                TakeoffAviary, env_kwargs=sa_env_kwargs, n_envs=1, seed=0
-            )
-        if env_name == "hover-aviary-v0":
-            eval_env = make_vec_env(
-                HoverAviary, env_kwargs=sa_env_kwargs, n_envs=1, seed=0
-            )
-        if env_name == "flythrugate-aviary-v0":
-            eval_env = make_vec_env(
-                FlyThruGateAviary, env_kwargs=sa_env_kwargs, n_envs=1, seed=0
-            )
-        if env_name == "tune-aviary-v0":
-            eval_env = make_vec_env(
-                TuneAviary, env_kwargs=sa_env_kwargs, n_envs=1, seed=0
-            )
-        eval_env = VecTransposeImage(eval_env)
 
-    #### Train the model #######################################
-    # checkpoint_callback = CheckpointCallback(save_freq=1000, save_path=filename+'-logs/', name_prefix='rl_model')
-    callback_on_best = StopTrainingOnRewardThreshold(
-        reward_threshold=EPISODE_REWARD_THRESHOLD, verbose=1
-    )
-    eval_callback = EvalCallback(
-        eval_env,
-        callback_on_new_best=callback_on_best,
-        verbose=1,
-        best_model_save_path=filename + "/",
-        log_path=filename + "/",
-        eval_freq=int(2000 / cpu),
-        deterministic=True,
-        render=False,
-    )
-    model.learn(
-        total_timesteps=steps,  # int(1e12),
-        callback=eval_callback,
-        log_interval=100,
-    )
+    def register_flythru_environment(self, ARGS):
+        self.environment_name = "flythru-aviary-v0"
 
-    #### Save the model ########################################
-    model.save(filename + "/success_model.zip")
-    print(filename)
+        register_env(
+            self.environment_name,
+            lambda _: FlyThruGateAviary(
+                aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
+                obs=ARGS.obs,
+                act=ARGS.act,
+            ),
+        )
 
-    #### Print training progression ############################
-    with np.load(filename + "/evaluations.npz") as data:
-        for j in range(data["timesteps"].shape[0]):
-            print(str(data["timesteps"][j]) + "," + str(data["results"][j][0]))
+        self.environment = FlyThruGateAviary(
+            aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
+            obs=ARGS.obs,
+            act=ARGS.act,
+            gui=ARGS.gui,
+            record=ARGS.record_video,
+        )
+
+    def register_takeoff_environment(self, ARGS):
+        self.environment_name = "takeoff-aviary-v0"
+
+        register_env(
+            self.environment_name,
+            lambda _: TakeoffAviary(
+                aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
+                obs=ARGS.obs,
+                act=ARGS.act,
+            ),
+        )
+
+        self.environment = TakeoffAviary(
+            aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
+            obs=ARGS.obs,
+            act=ARGS.act,
+            gui=ARGS.gui,
+            record=ARGS.record_video,
+        )
+
+    def build_tuner_config(self, ARGS):
+        self.tuner_config = ppo.DEFAULT_CONFIG.copy()
+
+        self.tuner_config = {
+            "env": self.environment_name,
+            "num_workers": 0 + ARGS.workers,
+            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+            "batch_mode": "complete_episodes",
+            "framework": "torch",
+        }
+
+    def build_tuner_stop_conditions(self):
+        self.tuner_stopper = {
+            "timesteps_total": 120000,
+            # "episode_reward_mean": 0,
+            # "training_iteration": 0,
+        }
+
+    def run_tuner(self, folder, config, stop):
+        self.tuner_results = tune.run(
+            PPOTrainer,
+            checkpoint_at_end=True,
+            config=config,
+            local_dir=folder,
+            stop=stop,
+            verbose=True,
+        )
+
+    def check_learning_achieved(self, results):
+        check_learning_achieved(results, 1.0)
+
+    def build_path_file(self, folder, results):
+        checkpoints = results.get_trial_checkpoints_paths(
+            trial=results.get_best_trial("episode_reward_mean", mode="max"),
+            metric="episode_reward_mean",
+        )
+        with open(folder + "/path.txt", "w+") as f:
+            f.write(checkpoints[0][0])
 
 
 if __name__ == "__main__":
-    #### Define and parse (optional) arguments for the script ##
-    parser = argparse.ArgumentParser(
-        description="Single agent reinforcement learning experiments script"
-    )
-    parser.add_argument(
-        "--env",
-        default=DEFAULT_ENV,
-        type=str,
-        choices=["takeoff", "hover", "flythrugate", "tune"],
-        help="Task (default: hover)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--algo",
-        default=DEFAULT_ALGO,
-        type=str,
-        choices=["ppo", "ddpg"],
-        help="RL agent (default: ppo)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--obs",
-        default=DEFAULT_OBS,
-        type=ObservationType,
-        help="Observation space (default: kin)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--act",
-        default=DEFAULT_ACT,
-        type=ActionType,
-        help="Action space (default: one_d_rpm)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--cpu",
-        default=DEFAULT_CPU,
-        type=int,
-        help="Number of training environments (default: 1)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--steps",
-        default=DEFAULT_STEPS,
-        type=int,
-        help="Number of training time steps (default: 35000)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--output_folder",
-        default=DEFAULT_OUTPUT_FOLDER,
-        type=str,
-        help='Folder where to save logs (default: "results")',
-        metavar="",
-    )
-    parser.add_argument(
-        "--gui",
-        default=True,
-        type=str2bool,
-        help="Whether to use PyBullet GUI (default: True)",
-        metavar="",
-    )
-    parser.add_argument(
-        "--record_video",
-        default=False,
-        type=str2bool,
-        help="Whether to record a video (default: False)",
-        metavar="",
-    )
-    ARGS = parser.parse_args()
-
-    run(**vars(ARGS))
+    SingleAgentPPO()
